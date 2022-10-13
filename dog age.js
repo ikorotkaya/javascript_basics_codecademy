42e6dbee09a295dbc53a703f0993a7abf42c1f5e
ikorotkaya <irinkakorotkaya@gmail.com>
2022-10-13 15:49:46 +0200

    learn switch statement

--- dog age.js
+++ dog age.js
@@ -36,3 +36,21 @@ let newton = celsius * (33/100);
 newton = Math.floor(newton);
 
 console.log(`The temperature is ${newton} degrees Newton.`);
+
+//switch keyword
+
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+  console.log('You get the gold medal!');
+  break;
+    case 'second place':
+  console.log('You get the silver medal!');
+  break;
+    case 'third place':
+  console.log('You get the bronze medal!');
+  break;
+  default:
+  console.log('No medal awarded.');
+  break;
+}
