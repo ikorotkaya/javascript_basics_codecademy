f181e59440d8dfa7d0283f44ab69db470a201c47
ikorotkaya <irinkakorotkaya@gmail.com>
2022-10-14 13:36:31 +0200

    create magic eight ball algorithm

--- dog age.js
+++ dog age.js
@@ -1,5 +1,6 @@
 // my age
 let myAge = 29;
+
 // dog's early years
 let earlyYears = 2;
 earlyYears *= 10.5;
@@ -17,6 +18,7 @@ let myName = 'Irina Korotkaya'.toLowerCase();
 
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
 
+
 // Kelvin Tmperature
 
 // constant variable
@@ -30,14 +32,17 @@ fahrenheit = Math.floor(fahrenheit);
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
+
 // Convert to Newton
+
 let newton = celsius * (33/100);
 // Round down
 newton = Math.floor(newton);
 
 console.log(`The temperature is ${newton} degrees Newton.`);
 
-//switch keyword
+
+//Switch keyword
 
 let athleteFinalPosition = 'first place';
 switch (athleteFinalPosition) {
@@ -54,3 +59,46 @@ switch (athleteFinalPosition) {
   console.log('No medal awarded.');
   break;
 }
+
+
+// Magice eight ball
+
+let userName = '';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'How long will I live?';
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(eightBall);
