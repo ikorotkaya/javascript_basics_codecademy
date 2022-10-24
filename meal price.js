bd9a78c219c3fb561cee17722e9d3b167e5595d0
ikorotkaya <irinkakorotkaya@gmail.com>
2022-10-24 12:53:52 +0200

    create a function to set a meal and price

---
+++ meal price.js
@@ -0,0 +1,26 @@
+const menu = {
+  _meal: "",
+  _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return (this._meal = mealToCheck);
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (this._price = priceToCheck);
+    }
+  },
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return "Today’s Special is Spaghetti for $5!";
+    } else {
+      return "Meal or price was not set correctly!";
+    }
+  },
+};
+
+menu.meal = "Pizza";
+menu.price = 8;
+
+console.log(menu.todaysSpecial);
