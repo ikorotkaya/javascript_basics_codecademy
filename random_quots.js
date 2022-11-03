2851e0709e6fe5ec0c0559305d7c8f35898229f2
ikorotkaya <irinkakorotkaya@gmail.com>
2022-11-03 11:24:49 +0100

    create random phrase generator

---
+++ random_quots.js
@@ -0,0 +1,20 @@
+const nouns = "skill king shirt debt birthday phone improvement speaker highway food song advertising perspective basket delivery".split(" ");
+
+const verbs = "wish lay affect bind extract persist manage prompt penetrate compose drink remove proclaim decorate divert".split(" ");
+
+const adjectives = "responsible grotesque rebel greedy laughable electric zonked graceful meek handy wicked new second scattered ubiquitous".split(" ");
+
+const adverbs = "dramatically positively hungrily less lively adventurously normally heavily arrogantly loosely shrilly widely deftly mechanically wetly".split(" ");
+
+
+const getRandomWord = (array) => {
+  randomWord = array[Math.floor(Math.random()*array.length)];
+  return randomWord;
+}
+
+const generateRandomPhrase = () => {
+  let randomPhrase = `${getRandomWord(adjectives)} ${getRandomWord(nouns)} ${getRandomWord(verbs)} ${getRandomWord(adverbs)}`;
+  return randomPhrase;
+};
+
+console.log(generateRandomPhrase());
