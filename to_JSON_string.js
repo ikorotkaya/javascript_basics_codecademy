849b5a57a343029b50fb66e5cdaae9716a601a07
ikorotkaya <irinkakorotkaya@gmail.com>
2022-12-12 22:09:21 +0100

    Learn JSON

---
+++ to_JSON_string.js
@@ -0,0 +1,9 @@
+const jsonData = '{"parent":{"name":"Sally","age":45,"children":[{"name":"Kim","age":3},{"name":"Lee","age":1}]}}';
+
+const jsObject = JSON.parse(jsonData);
+//change the age
+jsObject.parent.age = 35;
+
+const jsObjectToJson = JSON.stringify(jsObject);
+
+console.log(jsObjectToJson)
